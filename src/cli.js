e52f9ed6d2032847950a6d56a6049db12b18ae2b
hrvieira <luizhrvieira@outlook.com>
2023-06-26 11:00:23 -0300

    Refatorado

--- src/cli.js
+++ src/cli.js
@@ -4,21 +4,23 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
+function imprimeLista(resultado) {
+  console.log(chalk.blue('Lista de links:'), resultado);
+}
+
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
 
   if(fs.lstatSync(caminho).isFile()){
     const resultado = await pegaArquivo(argumentos[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+    imprimeLista(resultado);
 
   } else if (fs.lstatSync(caminho).isDirectory()){
     const arquivos = await fs.promises.readdir(caminho);
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      console.log(`${caminho}/${nomeDeArquivo}`);
-      console.log(lista);
+      imprimeLista(lista);
     })
-    console.log(arquivos);
   }
 
 }
