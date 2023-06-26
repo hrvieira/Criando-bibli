69e744dab3662b60be108d3c0f2c0f9481eb08c1
hrvieira <luizhrvieira@outlook.com>
2023-06-26 08:52:33 -0300

    Usando async/await e .then para trabalhar com promessas.

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'Não foi encontrado o arquivo.'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
-  fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch(trataErro)
+//  async/await
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
+
+//  Promises com .then
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro)
+// }
 
 // function pegaArquivo(caminhoDoArquivo) {
 //   const encoding = 'utf-8';
