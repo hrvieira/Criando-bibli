c19a664c268695992cabe7d5b0e2a6e7ee9753aa
hrvieira <luizhrvieira@outlook.com>
2023-06-26 08:33:59 -0300

    Primeiro commit - iniciando o projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vai precisar:
+
+-   nodeJS v18+
+-   Install package.json
+-   Chalk
---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não foi encontrado o arquivo.'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
+
+pegaArquivo('./arquivos/texto.md');
