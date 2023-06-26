ced30dd111fe54c70797ebd8402071086ee43194
hrvieira <luizhrvieira@outlook.com>
2023-06-26 10:11:33 -0300

    Código funcionando para captura de texto test. Iniciando para uma captura usando o terminal para informar de onde vai ser pego os links.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 -   nodeJS v18+
 -   Install package.json
--   Chalk
+-   Import Chalk
+
+## Links/Informações úteis
+
+-   Validações são importantes tanto no lado do front-end quanto do back-end. Você pode conferir duas libs para isso, **Joi**(https://joi.dev/) e **Yup**(https://www.npmjs.com/package/yup), e suas documentações com exemplos de uso. A Joi tem um **ambiente de testes**(https://joi.dev/tester/) que ajudam a visualizar melhor como utilizar os métodos dessa lib.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
+
+  return resultados;
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'Não foi encontrado o arquivo.'));
 }
@@ -10,9 +18,11 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro)
+  } finally {
+    console.log(chalk.yellow('operação concluída'));
   }
 }
 
